8594e00d377dec356aae53cad6fe38e5a491dbfa
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-13 16:02:00 +0300

    add: task 9 done

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Task 9'.
+'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''Inserts a new document.
+    '''
+
+    res = mongo_collection.insert_one(kwargs)
+    return res.inserted_id
