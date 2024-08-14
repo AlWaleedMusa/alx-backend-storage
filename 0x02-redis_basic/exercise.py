44379f50cb32c75d4760ac6d41dda2ce8b23f00d
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-14 23:25:29 +0300

    fix: task 1 refactored

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -39,29 +39,24 @@ class Cache:
         self._redis.set(random_key, data)
         return random_key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> str:
+    def get(
+        self,
+        key: str,
+        fn: Callable = None,
+    ) -> Union[str, bytes, int, float]:
         """
         Retrieve data from Redis and optionally apply a conversion function.
 
         Parameters:
-        - key: The key to retrieve the data.
-        - fn: An optional callable to convert the data.
+        - key: The key to retrieve the data (str).
+        - fn: An optional callable to convert the data (Callable).
 
         Returns:
-        - The retrieved data, optionally converted by fn.
+        - The retrieved data, optionally converted by fn. The data can be of type str, bytes, int, or float.
+        - If the key does not exist, it returns None.
         """
-        try:
-            data = self._redis.get(key)
-
-            if data is None:
-                return None
-
-            if fn:
-                return fn(data)
-            return data
-
-        except Exception as e:
-            return e
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
 
     def get_str(self, key: str) -> Optional[str]:
         """
